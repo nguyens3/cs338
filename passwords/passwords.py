69bc3413c170de8229e0e2707f279ef8dd8ab15b
nguyens3 <102560004+nguyens3@users.noreply.github.com>
2022-05-10 22:49:30 -0500

    Update passwords.py

--- passwords/passwords.py
+++ passwords/passwords.py
@@ -26,7 +26,35 @@ def cracked1():
   print(f"Hash: {hash_count}")
 
 
+def hash(password): 
+    encoded_password = password.encode('utf-8')
+    hasher = hashlib.sha256(encoded_password)
+    digest = hasher.digest()
+    digest_as_hex = binascii.hexlify(digest)
+    digest_as_hex_string = digest_as_hex.decode('utf-8') 
+    return digest_as_hex_string
+
+#Credit to Lily and Ashok for telling me to use a dictionary!
 def cracked2():
+  hash_count = 0
+  words = [line.strip().lower() for line in open('words.txt')] 
+  f = open("cracked2.txt", "w")
+  accounts = [line.strip().split(":") for line in open('passwords2.txt')]
+  dict_account = {}
+  for user in accounts:
+    dict_account[user[1]] = user[0]
+
+    for word in words:
+        for word2 in words:
+            combination = word + word2
+            hashed = hash(combination)
+            hash_count += 1
+            if(hashed in dict_account):
+                f.write(dict_account[hashed] + ":" + combination + "\n")
+  print(hash_count)
+  f.close()
+
+            
  
 def cracked3():
   words = [line.strip().lower() for line in open('words.txt')] 
